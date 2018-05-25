c0dc6b7c8f410e5cc3df7b93a3cd1ac9c0dc391d
1767123925@qq.com <a2926779426>
2018-05-25 15:37:54 +0800

    aa

--- src/main/java/web/Test1.java
+++ src/main/java/web/Test1.java
@@ -4,4 +4,11 @@ package web;
  * Created by Administrator on 2018/5/25.
  */
 public class Test1 {
+    public static void main(String[] args){
+        System.out.println("ceshi");
+    }
+    public  int getType(){
+
+        return 1;
+    }
 }
